9dc026f2021c16e2792b8b25c63bc6afbb383959
ujjwalchouhan <cujjwal315@gmail.com>
2024-06-30 09:05:25 +0530

    Update Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter '
 			style={{
 				background: 'linear-gradient(177.9deg, rgb(62 61 67) 3.6%, rgb(26 30 40) 50.8%)', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
-			height: "100%",
+			height: "80%",
 				width: "100%"
 			}}
 		>
