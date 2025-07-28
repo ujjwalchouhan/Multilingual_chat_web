b958c13f49920dea9cee2ca4789938493660666b
ujjwalchouhan <cujjwal315@gmail.com>
2025-07-28 20:32:59 +0530

    Update connectToMongoDB.js
    update database link

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect('mongodb+srv://cujjwal315:Sbc7ofPLwJxF65S8@cluster0.dmenfrs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+		await mongoose.connect('mongodb+srv://Vikas8055:Vikas123@cluster0.g4ce3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0%22');
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
